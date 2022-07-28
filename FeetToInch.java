d026d5140af35e6e90b55f8982d643db15b79b59
Sudarshan <pallesudarshanreddy2@gmail.com>
2022-07-28 14:32:24 +0530

    FeetToInchConvert logic

---
+++ FeetToInch.java
@@ -0,0 +1,8 @@
+public class FeetToInch {
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        feet = s.nextInt();
+        System.out.print("Inches:"+feet*12);
+    }
+}
